cff7e8fda203296715f482d07139fc99177f8b82
Kamil <unnamedxaer@gmail.com>
2021-05-17 18:14:40 +0200

    update email regexp

--- validation/contact.js
+++ validation/contact.js
@@ -1,4 +1,4 @@
-const emailRegexpString = `/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/`;
+const emailRegexpString = "^[-!#$%&'*+/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z{|}~])*@[a-zA-Z](-?[a-zA-Z0-9])*(\.[a-zA-Z](-?[a-zA-Z0-9])*)+$";
 
 const emailRegexp = new RegExp(emailRegexpString);
 
