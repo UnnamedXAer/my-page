4cbdf39009e3405409ccab7b7d76a9022db1aacf
UnnamedXAer <UnnamedXAer@gmail.com>
2021-05-19 10:55:37 +0200

    redesign about me section
    -makes text to wrap arount logo;

--- state/state.js
+++ state/state.js
@@ -136,8 +136,8 @@ async function fetchAboutMe() {
 	console.log(new Date().toUTCString(), 'about to fetch about me');
 	try {
 		return {
-			fetchedAt: new Date(2000, 1, 1),
-			text: ''
+			fetchedAt: new Date(),
+			text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
 		};
 	} catch (err) {
 		console.error(new Date().toUTCString(), 'fetch about me:', err);
